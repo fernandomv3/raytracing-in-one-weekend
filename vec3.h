67b416df66ff24dba89fcd13f88bce53e525b2f8
fernandomv3 <fernandomv3@gmail.com>
2017-10-05 14:57:11 -0500

    chapter9

--- vec3.h
+++ vec3.h
@@ -86,6 +86,17 @@ inline vec3 unit_vector(vec3 v){
   return v / v.length();
 }
 
+inline bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted){
+  vec3 uv = unit_vector(v);
+  float dt = dot(uv,n);
+  float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
+  if(discriminant > 0){
+    refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
+    return true;
+  }else
+    return false;
+}
+
 inline vec3& vec3::operator+=(const vec3& v2){
   this->e[0] += v2.e[0];
   this->e[1] += v2.e[1];
